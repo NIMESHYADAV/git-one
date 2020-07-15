bae2a9302e934336e2ec3acf685546e1d76004b1
NimeshYadav <nimeshyadav780@gmail.com>
2020-07-15 14:34:08 +0530

    Commiting files

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # This is the test file for understanding the GITHUB
 print("Hi Nimesh")
+<<<<<<< HEAD
 print("Hello World")
 var = "My Second Changes"
 print(var)
@@ -7,3 +8,6 @@ print(var)
 def sum(a,b):
 	return(a+b)
 print(sum(4,5))
+=======
+print("Hello World")
+>>>>>>> parent of 4839363... Added var to store the string and printed it
